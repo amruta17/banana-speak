dfb2270e8a32a11685367f86124b80a80e43d67c
amruta <pa17april@gmail.com>
2022-02-02 13:02:28 +0530

    output processing done

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log("banana talk");
+// var username=prompt("what is your name?");
+// alert("hello " + username);
+var btn=document.querySelector("#btn");
+var txtArea=document.querySelector(".txt-area");
+var output=document.querySelector(".output");
+
+
+
+btn.addEventListener("click", function(){
+    console.log("clicked");
+    console.log(txtArea.value);
+    console.log(output.innerText=txtArea.value+txtArea.value);
+})
+
+
+
+
