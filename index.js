baa3a2fa67f8b38bf9a868ec2cf582aa7e270b4d
amruta <pa17april@gmail.com>
2022-02-09 17:50:27 +0530

    docs:server

--- index.js
+++ index.js
@@ -4,14 +4,27 @@
 var btn=document.querySelector("#btn");
 var txtArea=document.querySelector(".txt-area");
 var output=document.querySelector(".output");
+// var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+// function getTransalationURL(text){
+//     return serverURL +"?" +"text"+ text;
+// }
 
+function clickHandler(){
+ console.log(output.innerText="ba"+txtArea.value) ;
+// var inputText=txtArea.value;//taking input
 
-btn.addEventListener("click", function(){
-    console.log("clicked");
-    console.log(txtArea.value);
-     console.log(output.innerText="ba"+txtArea.value);
-})
+//     fetch(getTransalationURL(inputText))
+//     .then(Response =>Response.json())
+//     .then(json => console.log(json))
+}
+
+// btn.addEventListener("click", function(){
+//     console.log("clicked");
+//     console.log(txtArea.value);
+//      console.log(output.innerText="ba"+txtArea.value);
+// })
+btn.addEventListener("click", clickHandler)
 
 
 
