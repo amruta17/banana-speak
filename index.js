7123e46005fad2c1b250f47b897dd4ae74abcd35
amruta <pa17april@gmail.com>
2022-02-10 13:38:46 +0530

    docs: completed

--- index.js
+++ index.js
@@ -6,24 +6,30 @@ var txtArea=document.querySelector(".txt-area");
 var output=document.querySelector(".output");
 // var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-// function getTransalationURL(text){
-//     return serverURL +"?" +"text"+ text;
+//  function getTransalationURL(text){
+//     return serverURL +"?" +"text"+ "=" + text;
+// }
+// function errorHandeler(error){
+//     console.log("error occured");
+//     alert("something wrong with server..try again ater sometimw!..")
 // }
 
 function clickHandler(){
- console.log(output.innerText="ba"+txtArea.value) ;
+ console.log(output.innerText=txtArea+ txtArea.value); //important in case of server is not there comment all server art and recomment is you will see the translated output
 // var inputText=txtArea.value;//taking input
 
-//     fetch(getTransalationURL(inputText))
-//     .then(Response =>Response.json())
-//     .then(json => console.log(json))
-}
+//    fetch(getTransalationURL(inputText))
+//    .then(Response =>Response.json())
+//    .then(json => console.log(json))
+//    .catch(errorHandeler)
+// }
 
 // btn.addEventListener("click", function(){
 //     console.log("clicked");
 //     console.log(txtArea.value);
 //      console.log(output.innerText="ba"+txtArea.value);
-// })
+// 
+}
 btn.addEventListener("click", clickHandler)
 
 
