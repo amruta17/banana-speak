fea2f9403e9d82041980daa85a0144b57ff96d1e
amruta <pa17april@gmail.com>
2022-02-10 14:08:14 +0530

    docs:minion

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
+var btn = document.querySelector("#btn");
+var tvxtArea = document.querySelector(".txt-area");
+var output = document.querySelector(".output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-var btn=document.querySelector("#btn");
-var txtArea=document.querySelector(".txt-area");
-var output=document.querySelector(".output");
-var serverURL="https://api.funtranslations.com/translate/minion.json";
-
- function getTransalationURL(text){
-    return serverURL +"?" +"text"+ "=" + text;
+function getTransalationURL(text) {
+    return serverURL + "?" + "text" + "=" + text;
 }
-function errorHandeler(error){
+
+function errorHandeler(error) {
     console.log("error occured");
     alert("something wrong with server..try again ater sometimw!..")
 }
 
-function clickHandler(){
-var inputText=txtArea.value; //taking input
+function clickHandler() {
+    var inputText = txtArea.value; //taking input
 
-   fetch(getTransalationURL(inputText))
-   .then(Response =>Response.json())
-   .then(json => {
-       var translatedText= json.contents.translated;
-    output.innerText=translatedText;
-    })
-   .catch(errorHandeler)
+    fetch(getTransalationURL(inputText))
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandeler)
 }
-
-
 
 btn.addEventListener("click", clickHandler)
 
@@ -35,7 +33,7 @@ btn.addEventListener("click", clickHandler)
 //     console.log(txtArea.value);
 //      console.log(output.innerText="ba"+txtArea.value);
 // 
- //console.log(output.innerText=txtArea+ txtArea.value); //important in case of server is not there comment all server art and recomment is you will see the translated output
+//console.log(output.innerText=txtArea+ txtArea.value); //important in case of server is not there comment all server art and recomment is you will see the translated output
 // console.log("banana talk");
 // var username=prompt("what is your name?");
 // alert("hello " + username);
